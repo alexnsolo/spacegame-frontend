b53004e90f33fe7995b721664b0228690e7d7923
forana <alex@catalyze.io>
2015-01-10 18:36:32 -0600

    present this

--- app.js
+++ app.js
@@ -79,28 +79,245 @@ $(document).ready(function() {
       return PewNet.requestGame(username, function(gameId, player, players) {
         game.show();
         form.hide();
-        window.Game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
+        return window.Game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
           preload: function() {
-            Game.load.image('ship', 'images/ship-spark.png');
-            return Game.load.image('hex', 'images/hexagon.png');
+            Game.load.image('ship-fighter', 'images/ship-fighter.png');
+            Game.load.image('ship-bomber', 'images/ship-bomber.png');
+            Game.load.image('ship-carrier', 'images/ship-carrier.png');
+            Game.load.image('ship-frigate', 'images/ship-frigate.png');
+            Game.load.image('bullet', 'images/bullet.png');
+            Game.load.image('hex', 'images/hexagon.png');
+            return Game.load.image('explosion', 'images/explosion.png');
           },
           create: function() {
-            var grid, ship;
-            grid = new HexGrid(10, 10);
-            return ship = Game.add.sprite(200, 200, 'ship');
+            var hexGrid, ship1, ship2;
+            hexGrid = new HexGrid(10, 10);
+            ship1 = new Ship(1, hexGrid.tiles[7]);
+            ship2 = new Ship(2, hexGrid.tiles[23]);
+            ship1.targetShip(ship2);
+            ship2.targetShip(ship1);
+            setTimeout((function(_this) {
+              return function() {
+                return ship1.moveToTile(hexGrid.tiles[8]);
+              };
+            })(this), 3000);
+            setTimeout((function(_this) {
+              return function() {
+                return ship2.moveToTile(hexGrid.tiles[22]);
+              };
+            })(this), 2000);
+            return PewNet.joinGame(player, gameId, {
+              shipCreate: function(ship) {
+                var tile;
+                console.log('server: shipCreate', arguments);
+                tile = hexGrid.findTileByCoords(ship.x, ship.y);
+                if (!tile) {
+                  console.log('no tile! fuck');
+                }
+                return new Ship(ship.id, tile);
+              },
+              shipMove: function(id, coordinates) {
+                return console.log('server: shipMove', arguments);
+              }
+            }, function(messager) {
+              return window.dg = messager;
+            });
           }
-        });
-        return PewNet.joinGame(player, gameId, {
-          shipMove: function() {
-            return console.log(arguments);
-          }
-        }, function(messager) {
-          return window.dg = messager;
         });
       });
     }
   });
 });
+
+var Bullet,
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+Bullet = (function() {
+  Bullet.sprite = null;
+
+  Bullet.target = null;
+
+  Bullet.damage = null;
+
+  function Bullet(startPos, target) {
+    this.onShipCollide = __bind(this.onShipCollide, this);
+    this.damage = _.sample([17, 10, 6]);
+    this.target = target;
+    this.sprite = Game.add.sprite(startPos.x, startPos.y, 'bullet');
+    this.sprite.anchor.set(0.5);
+    this.sprite.scale = {
+      x: 0.4,
+      y: 0.4
+    };
+    Game.physics.arcade.enableBody(this.sprite);
+    this.sprite.body.mass = 0;
+    this.sprite.update = (function(_this) {
+      return function() {
+        if (!_this.target.sprite.alive) {
+          _this.sprite.kill();
+        }
+        Game.physics.arcade.moveToObject(_this.sprite, _this.target.sprite.position, 200);
+        return Game.physics.arcade.collide(_this.sprite, _this.target.sprite, _this.onShipCollide);
+      };
+    })(this);
+  }
+
+  Bullet.prototype.onShipCollide = function(me, other) {
+    other.damage(this.damage);
+    return this.sprite.kill();
+  };
+
+  return Bullet;
+
+})();
+
+var Explosion;
+
+Explosion = (function() {
+  Explosion.prototype.sprite = null;
+
+  function Explosion(position) {
+    this.sprite = Game.add.sprite(position.x, position.y, 'explosion');
+    this.sprite.anchor.set(0.5);
+    this.sprite.scale = {
+      x: 0,
+      y: 0
+    };
+    this.sprite.update = (function(_this) {
+      return function() {
+        _this.sprite.rotation += 0.01;
+        _this.sprite.scale.x += 0.05;
+        return _this.sprite.scale.y += 0.05;
+      };
+    })(this);
+    setTimeout((function(_this) {
+      return function() {
+        return _this.sprite.kill();
+      };
+    })(this), 500);
+  }
+
+  return Explosion;
+
+})();
+
+var HexGrid;
+
+HexGrid = (function() {
+  HexGrid.prototype.tiles = [];
+
+  function HexGrid(sizeX, sizeY) {
+    var tile, x, y, _i, _j;
+    for (x = _i = 0; 0 <= sizeX ? _i <= sizeX : _i >= sizeX; x = 0 <= sizeX ? ++_i : --_i) {
+      for (y = _j = 0; 0 <= sizeY ? _j <= sizeY : _j >= sizeY; y = 0 <= sizeY ? ++_j : --_j) {
+        if (!((Helpers.isEven(x) && Helpers.isEven(y)) || (Helpers.isOdd(x) && Helpers.isOdd(y)))) {
+          tile = new HexTile(x, y);
+          this.tiles.push(tile);
+        }
+      }
+    }
+  }
+
+  HexGrid.prototype.findTileByCoords = function(x, y) {
+    return _.find(tiles, function(tile) {
+      return tile.x === x && tile.y === y;
+    });
+  };
+
+  return HexGrid;
+
+})();
+
+var HexTile;
+
+HexTile = (function() {
+  function HexTile(x, y) {
+    this.coordinates = {
+      x: x,
+      y: y
+    };
+    this.sprite = Game.add.sprite(x * 120, y * 69, 'hex');
+    this.sprite.anchor.set(0.5);
+  }
+
+  HexTile.prototype.sprite = null;
+
+  HexTile.prototype.coordinates = null;
+
+  return HexTile;
+
+})();
+
+var Player;
+
+Player = (function() {
+  function Player() {}
+
+  return Player;
+
+})();
+
+var Ship;
+
+Ship = (function() {
+  Ship.prototype.id = null;
+
+  Ship.prototype.sprite = null;
+
+  Ship.prototype.tile = null;
+
+  Ship.prototype.target = null;
+
+  function Ship(id, starting_tile) {
+    var x, y;
+    this.id = id;
+    this.tile = starting_tile;
+    x = this.tile.sprite.position.x;
+    y = this.tile.sprite.position.y;
+    this.sprite = Game.add.sprite(x, y, _.sample(['ship-fighter', 'ship-bomber', 'ship-frigate', 'ship-carrier']));
+    this.sprite.scale = {
+      x: 0.15,
+      y: 0.15
+    };
+    this.sprite.rotation = _.sample([10, 382, 133, 98]);
+    this.sprite.anchor.set(0.5);
+    this.sprite.health = 50;
+    this.sprite.events.onKilled.add((function(_this) {
+      return function() {
+        return new Explosion(_this.sprite.position);
+      };
+    })(this));
+    Game.physics.arcade.enableBody(this.sprite);
+    setInterval((function(_this) {
+      return function() {
+        if (_this.target && _this.sprite.alive) {
+          return _this.fireAt(_this.target);
+        }
+      };
+    })(this), 1000);
+  }
+
+  Ship.prototype.moveToTile = function(tile) {
+    this.tile = tile;
+    this.sprite.position.x = tile.sprite.position.x;
+    return this.sprite.position.y = tile.sprite.position.y;
+  };
+
+  Ship.prototype.targetShip = function(ship) {
+    return this.target = ship;
+  };
+
+  Ship.prototype.fireAt = function(ship) {
+    var bullet;
+    if (!ship.sprite.alive) {
+      return;
+    }
+    return bullet = new Bullet(this.sprite.position, ship);
+  };
+
+  return Ship;
+
+})();
 
 var HexGrid;
 
